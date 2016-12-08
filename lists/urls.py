70ee91888fda95732d4015745d1dcbc2182ff787
Carlos Eduardo Berton <berton.5b@gmail.com>
2016-12-08 09:24:14 -0200

    List and detail urls

---
+++ lists/urls.py
@@ -0,0 +1,10 @@
+from django.conf.urls import url
+from django.views.generic import TemplateView
+
+from lists.views import TodoList, ListDetail, TaskDetail
+
+urlpatterns = [
+    # api
+    url(r'^$', TodoList.as_view(), name="todo_list"),
+    url(r'^(?P<pk>[0-9]+)/$', ListDetail.as_view(), name="list_detail")
+]
