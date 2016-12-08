4479f4b4ae3b81d751fe6d3dffd5afeeaa3d47be
Carlos Eduardo Berton <berton.5b@gmail.com>
2016-12-08 09:14:35 -0200

    Null fields and update method save

--- lists/models.py
+++ lists/models.py
@@ -1,5 +1,7 @@
 from __future__ import unicode_literals
 
+import datetime
+
 from django.db import models
 from django.contrib.auth.models import User
 
@@ -10,19 +12,19 @@ class List(models.Model):
 	def __str__(self):
 		return self.title
 
-	def incomplete_itens(self):
+	def get_incomplete_itens(self):
 		return Item.objects.filter(todo_list=self, completed=None)
 
 
 class Item(models.Model):
 	title = models.CharField(max_length=180)
-	todo_list = models.ForeignKey(List)
-	assigned_to = models.ForeignKey(User, on_delete=models.CASCADE, related_name='assigned_to')
+	todo_list = models.ForeignKey(List, related_name='tasks', on_delete=models.CASCADE)
+	assigned_to = models.ForeignKey(User, blank = True, null = True, on_delete=models.CASCADE, related_name='assigned_to')
 	priority = models.IntegerField(default=5)
-	completed = models.BooleanField(default=None)
+	completed = models.NullBooleanField(default=None)
 	completed_date = models.DateTimeField(blank=True, null=True)
-	created_on = models.DateTimeField(auto_now_add=True)
-	created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='created_by')
+	created_on = models.DateTimeField()
+	created_by = models.ForeignKey(User, blank = True, null = True, on_delete=models.CASCADE, related_name='created_by')
 
 	class Meta:
 		ordering = ['-priority', 'created_on']
@@ -31,7 +33,9 @@ class Item(models.Model):
 		return self.title
 
 	# Set completed
-	def save(self):
+	def save(self, **kwargs):
+		if not self.created_on:
+			self.created_on = datetime.datetime.now()
 		if self.completed:
 			self.completed_date = datetime.datetime.now()
 		
