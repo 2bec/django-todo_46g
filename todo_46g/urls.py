b05704c88448fbe8077f15b52c04195d6c242a8e
Carlos Eduardo Berton <berton.5b@gmail.com>
2016-12-09 17:28:51 -0200

    Included api urls

--- todo_46g/urls.py
+++ todo_46g/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.conf.urls import url, include
 from django.contrib import admin
 from django.conf import settings
 from django.views.generic import TemplateView
@@ -22,4 +22,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^$', TemplateView.as_view(template_name="react/index.html")),
+    url(r'^todo-list/', include("lists.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
