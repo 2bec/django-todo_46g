22226eae796d4c904b68fef47a343d8ad713f40c
Carlos Eduardo Berton <berton.5b@gmail.com>
2016-12-09 20:10:41 -0200

    Tasks views

--- lists/views.py
+++ lists/views.py
@@ -66,3 +66,47 @@ class ListDetail(APIView):
         todo_list = self.get_object(pk)
         todo_list.delete()
         return Response(todo_list)
+
+
+class TasksList(APIView):
+    """
+    List all, or create a new one.
+    """
+    def get(self, request, format=None):
+        # Podemos realizar filtos no resultado
+        tasks = get_list_or_404(Item)
+        serializer = ItemSerializer(tasks, many=True, context={'request':request})
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = ItemSerializer(data=request.data, context={'request':request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class TaskDetail(APIView):
+    """
+    Retrieve, update or delete a instance.
+    """
+    def get_object(self, pk):
+        return get_object_or_404(Item, pk=pk)
+
+    def get(self, request, pk, format=None):
+        task = self.get_object(pk)
+        serializer = ItemSerializer(task, context={'request':request})
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        task = self.get_object(pk)
+        serializer = ItemSerializer(task, data=request.data, partial=True, context={'request':request})
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        task = self.get_object(pk)
+        task.delete()
+        return Response(task)
