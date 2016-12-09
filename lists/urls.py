daf90e39fa0e3dff4b03b7d55879b45e70d0c02c
Carlos Eduardo Berton <berton.5b@gmail.com>
2016-12-09 17:34:34 -0200

    Removed import to not used views (clean)

--- lists/urls.py
+++ lists/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
 
-from lists.views import TodoList, ListDetail, TaskDetail
+from lists.views import TodoList, ListDetail
 
 urlpatterns = [
     # api
