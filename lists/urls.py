aad12906c1208bba0a5f532c04948ad2bf0069d5
Carlos Eduardo Berton <berton.5b@gmail.com>
2016-12-09 20:11:01 -0200

    Task detail url

--- lists/urls.py
+++ lists/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
 
-from lists.views import TodoList, ListDetail
+from lists.views import TodoList, ListDetail, TaskDetail
 
 urlpatterns = [
     # api
     url(r'^$', TodoList.as_view(), name="todo_list"),
-    url(r'^(?P<pk>[0-9]+)/$', ListDetail.as_view(), name="list_detail")
+    url(r'^(?P<pk>[0-9]+)/$', ListDetail.as_view(), name="list_detail"),
+    url(r'^task/(?P<pk>[0-9]+)/$', TaskDetail.as_view(), name="task_detail"),
 ]
