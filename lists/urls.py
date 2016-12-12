365d24ed7ce71965271832bee3deb6585675489e
Carlos Eduardo Berton <berton.5b@gmail.com>
2016-12-11 23:34:21 -0200

    Added cache with time to expiry

--- lists/urls.py
+++ lists/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 from django.views.generic import TemplateView
+from django.views.decorators.cache import cache_page
 
 from lists.views import TodoList, ListDetail, TaskDetail
 
 urlpatterns = [
     # api
-    url(r'^$', TodoList.as_view(), name="todo_list"),
-    url(r'^(?P<pk>[0-9]+)/$', ListDetail.as_view(), name="list_detail"),
-    url(r'^task/(?P<pk>[0-9]+)/$', TaskDetail.as_view(), name="task_detail"),
+    url(r'^$', cache_page(60 * 12)(TodoList.as_view()), name="todo_list"),
+    url(r'^(?P<pk>[0-9]+)/$', cache_page(60 * 12)(ListDetail.as_view()), name="list_detail"),
+    url(r'^task/(?P<pk>[0-9]+)/$', cache_page(60 * 12)(TaskDetail.as_view()), name="task_detail"),
 ]
